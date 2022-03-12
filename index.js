6160671fba4ed327a61c66d897536c27198cacf2
Charlotte Walker <charlielizzy@me.com>
2022-03-12 12:23:08 +0000

    round to 2 decimal places

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const toggle = document.getElementById("toggle");
 
 toggle.addEventListener("change", () => {
-
-    if (toggle.checked === true) {
+  if (toggle.checked === true) {
     const slider = document.getElementById("slider");
     slider.addEventListener("input", (event) => {
-    const sliderValue = Number(event.target.value);
-    const cost = document.getElementById("cost");
-    cost.innerText = `$${sliderValue*0.75}.00`;
-    const pageviews = document.getElementById("pageviews");
-    pageviews.innerText = `${sliderValue*6.25}k pageviews`;
-})
-} else {
+      let sliderValue = Number(event.target.value);
+      console.log(sliderValue);
+      const cost = document.getElementById("cost");
+      cost.innerText = `$${(sliderValue * 0.75).toFixed(2)}`;
+      const pageviews = document.getElementById("pageviews");
+      pageviews.innerText = `${sliderValue * 6.25}k pageviews`;
+    });
+  } else {
     const slider = document.getElementById("slider");
-slider.addEventListener("input", (event) => {
-    const sliderValue = Number(event.target.value);
-    const cost = document.getElementById("cost");
-    cost.innerText = `$${sliderValue}.00`;
-    const pageviews = document.getElementById("pageviews");
-    pageviews.innerText = `${sliderValue*6.25}k pageviews`;
-})
-}})
+    slider.addEventListener("input", (event) => {
+      const sliderValue = Number(event.target.value);
+      const cost = document.getElementById("cost");
+      cost.innerText = `$${sliderValue.toFixed(2)}`;
+      const pageviews = document.getElementById("pageviews");
+      pageviews.innerText = `${sliderValue * 6.25}k pageviews`;
+    });
+  }
+});
