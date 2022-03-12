845ff0f8fee12b88bdf2d2e2a5ae9c5bafb95b89
Charlotte Walker <charlielizzy@me.com>
2022-03-12 10:24:53 +0000

    add event listener

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const slider = document.getElementById("slider");
+const toggle = document.getElementById("toggle");
+
+toggle.addEventListener("change", () => {
+
+    if (toggle.checked === true) {
+    const slider = document.getElementById("slider");
+    slider.addEventListener("input", (event) => {
+    const sliderValue = Number(event.target.value);
+    const cost = document.getElementById("cost");
+    cost.innerText = `$${sliderValue*0.75}.00`;
+    const pageviews = document.getElementById("pageviews");
+    pageviews.innerText = `${sliderValue*6.25}k pageviews`;
+})
+} else {
+    const slider = document.getElementById("slider");
 slider.addEventListener("input", (event) => {
     const sliderValue = Number(event.target.value);
     const cost = document.getElementById("cost");
     cost.innerText = `$${sliderValue}.00`;
     const pageviews = document.getElementById("pageviews");
-    pageviews.innerText = `${sliderValue*6.25}k pageviews`
+    pageviews.innerText = `${sliderValue*6.25}k pageviews`;
 })
+}})
