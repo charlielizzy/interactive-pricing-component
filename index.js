9e78aa8816ae30a37bcc3e9f6f654c93860b20a9
Charlotte Walker <charlielizzy@me.com>
2022-04-12 11:07:36 +0100

    recent commit

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 const toggle = document.getElementById("toggle");
+const slider = document.getElementById("slider");
+const cost = document.getElementById("cost");
+const pageviews = document.getElementById("pageviews");
 
-toggle.addEventListener("change", () => {
+window.addEventListener("load", (event) => {
   if (toggle.checked === true) {
-    const slider = document.getElementById("slider");
     slider.addEventListener("input", (event) => {
       let sliderValue = Number(event.target.value);
-      console.log(sliderValue);
-      const cost = document.getElementById("cost");
       cost.innerText = `$${(sliderValue * 0.75).toFixed(2)}`;
-      const pageviews = document.getElementById("pageviews");
       pageviews.innerText = `${sliderValue * 6.25}k pageviews`;
     });
   } else {
-    const slider = document.getElementById("slider");
     slider.addEventListener("input", (event) => {
       const sliderValue = Number(event.target.value);
-      const cost = document.getElementById("cost");
       cost.innerText = `$${sliderValue.toFixed(2)}`;
-      const pageviews = document.getElementById("pageviews");
+      pageviews.innerText = `${sliderValue * 6.25}k pageviews`;
+    });
+  }
+});
+
+toggle.addEventListener("change", () => {
+  if (toggle.checked === true) {
+    slider.addEventListener("input", (event) => {
+      let sliderValue = Number(event.target.value);
+      cost.innerText = `$${(sliderValue * 0.75).toFixed(2)}`;
+      pageviews.innerText = `${sliderValue * 6.25}k pageviews`;
+    });
+  } else {
+    slider.addEventListener("input", (event) => {
+      const sliderValue = Number(event.target.value);
+      cost.innerText = `$${sliderValue.toFixed(2)}`;
       pageviews.innerText = `${sliderValue * 6.25}k pageviews`;
     });
   }
